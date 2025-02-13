d1c13117a9d4bea875a92863b40baea56cf9890f
kumaru <kumaru@gamil>
2025-02-13 17:36:30 +0530

    new chnages vpc

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.86.1"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "5.86.1"
+#     }
+#   }
+# }
 
 provider "aws" {
   # Configuration options
